09c0a4df9c5ff64b1d825f9c7dadccf888ccec38
gogus4 <diego.dacosta@hotmail.fr>
2014-09-18 17:44:22 +0200

    First commit : index.php using Metro BootStrap

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="product" content="Five Mercredi">
+        <meta name="description" content="Site permettant de s'enregistrer aux matchs de five tous les mercredi">
+        <meta name="author" content="Diego Da Costa Oliveira, France, Paris">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/metro-bootstrap.css">
+        <link rel="stylesheet" href="css/icon-font.css">
+
+        <!-- Script JS -->
+        <script src="js/jquery/jquery.min.js"></script>
+        <script src="js/jquery/jquery.widget.min.js"></script>
+        <script src="js/metro/metro.min.js"></script>
+
+        <title>Le Five Inscription - Accueil</title>
+    </head>
+
+    <body class="metro">
+        <nav class="navigation-bar fixed-top">
+            <div class="navigation-bar-content container" style="margin-left:15%;">            
+                <div class="element">
+                    <span class="icon-trophy"></span>
+                    LE FIVE INSCRIPTION
+                </div>
+
+                <span class="element-divider"></span>
+                <a class="element brand" href="index.php" title="Accueil"><span class="icon-home"></span></a>
+                <span class="element-divider"></span>
+
+                <span class="element-divider"></span>
+                <a class="element brand" href="common/stats.php" title="Statistiques"><span class="icon-stats"></span></a>
+                <span class="element-divider"></span>
+
+                <span class="element-divider"></span>
+                <a class="element brand" href="common/calendar.php" title="Calendrier"><span class="icon-calendar"></span></a>
+                <span class="element-divider"></span>
+
+                <div class="element input-element">
+                    <form>
+                        <div class="input-control text size3">
+                            <input type="text" style="width:70%:" placeholder="Rechercher un joueur ...">
+                            <button class="btn-search"></button>
+                        </div>
+                    </form>
+                </div>
+
+                <span class="element-divider place-right"></span>
+
+                <div class="element place-right">
+                    <span class="icon-user"></span>
+                    <a class="dropdown-toggle" href="#"><small><strong>Diego Da Costa Oliveira</strong></small></a>
+
+                    <ul class="dropdown-menu bg-cyan" data-role="dropdown">
+                        <li><a href="profil.php" class="fg-white" title="Profil"><small><strong><span class="icon-user"></span> Profil</small></strong></a></li>
+                        <li><a href="logout.php" class="fg-white" title="Deconnexion"><small><strong><span class="icon-locked"></span> Deconnexion</small></strong></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div>
+<div style="background: #31190C top left no-repeat; background-size: cover; height: 300px;">
+<div class="container" style="padding: 50px 20px">
+<h1 class="fg-white">Metro UI CSS 2.0</h1>
+            <image src="images/lefive.png" style="margin-top:50px;height:200px;width=200px;">
+
+</div>
+</div>
+
+        </div>
+    </body>
+</html>
